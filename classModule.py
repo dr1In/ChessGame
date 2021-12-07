351bbfb86b2870a6852f227a10fe1167c1b61e4f
dr1In <85993985+dr1In@users.noreply.github.com>
2021-12-07 13:08:12 +0300

    pawn movement

--- classModule.py
+++ classModule.py
@@ -6,14 +6,14 @@ y_axis = ['1', '2', '3', '4', '5', '6', '7', '8']
 
 class Board():
     def __init__(self, coords: dict()):
-        self.fugeres_coords = coords
+        self.figures_coords = coords
     
     def show(self):
         for_show = [[None for _ in range(8)] for __ in range(8)]
         for y in range(8):
             for x in range(8):
-                if self.fugeres_coords[y_axis[y]][x_axis[x]] is None: for_show[y][x] = '   '
-                else: for_show[y][x] = self.fugeres_coords[y_axis[y]][x_axis[x]].get_symbol()
+                if self.figures_coords[y_axis[y]][x_axis[x]] is None: for_show[y][x] = '   '
+                else: for_show[y][x] = self.figures_coords[y_axis[y]][x_axis[x]].get_symbol()
         Board_view_decorate(for_show)
         
     def get_figures_coords(self):
@@ -22,11 +22,13 @@ class Board():
     def get_kings_position(self):
         pass
 
-    def get_figure_type_on_place(self, coords: dict()):
-        pass
+    def get_figure_type_on_place(self, place: str()):
+        return self.figures_coords[place[-1]][place[0]].get_type()
 
-    def change_places(self, new: str()):
-        pass
+    def swap(self, cur_place: str(), new_place: str(), kill: bool()):
+        if not kill:
+            self.figures_coords[new_place[-1]][new_place[0]] = self.figures_coords[cur_place[-1]][cur_place[0]]
+            self.figures_coords[cur_place[-1]][cur_place[0]] = None
 
 
 class Figure():
@@ -87,7 +89,22 @@ def Board_view_decorate(current_view: list()):
     print('     A   B   C   D   E   F   G   H  ')    
 
 
-def Pawn_movement(current_place: dict(), team: str(), all_coords: list()):
-    moves = []
+def Pawn_movement(place: dict(), team: str(), all_coords: list()):
+    (x, y, moves) = (place[0], int(place[-1]), [])
+    if team == 'white':
+        if y == 7:
+            if all_coords[f'{y - 1}'][x] is None and all_coords[f'{y - 2}'][x] is None:
+                moves.append(x + f'{y - 1}')
+                moves.append(x + f'{y - 2}')
+        if y < 7 and all_coords[f'{y - 1}'][x] is None:
+            moves.append(x + f'{y - 1}')
+    else:
+        if y == 2:
+            if all_coords[f'{y + 1}'][x] is None and all_coords[f'{y + 2}'][x] is None:
+                moves.append(x + f'{y + 1}')
+                moves.append(x + f'{y + 2}')
+        if y > 2 and all_coords[f'{y + 1}'][x] is None:
+            moves.append(x + f'{y + 1}')
+
 
     return moves
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+import os
 from classModule import *
-from os import system
+from os import kill, system
 
 def main():
     team_now = 'black'
     Running = True
     board = Board(First_realize())
     board.show()
+    
+    movements = {
+        'pawn': Pawn_movement
+    }
+
+
+    #print(board.get_figures_coords())
     print('Пример хода: a2 a4')
 
     while Running:
@@ -13,8 +21,13 @@ def main():
         else: team_now = 'black'
 
         cur_place, want = map(str, input('Ход команды {}: '.format(team_now)).split())
-        print(cur_place, want)
-            
+        while want not in movements[board.get_figure_type_on_place(cur_place)](cur_place, team_now, board.get_figures_coords()):
+            print('Невозможный ход, повторите попытку: ')
+            cur_place, want = map(str, input().split())
+        else: board.swap(cur_place, want, False)
+
+        system('CLS')
+        board.show()
 
     print('Игра окончена')
 
